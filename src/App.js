a61a324f14d7d6d3bbe146922bff7db82bccc7e3
shahirulprojects <shahiruliimalo@gmail.com>
2024-05-16 05:10:36 +0800

    fix:reconfigure API

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import "./App.css";
 import SearchIcon from "./search.svg";
 import MovieCard from "./MovieCard";
 
-const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=c45e02c3";
-
 const App = () => {
-  const [movies, setMovies] = useState([]); //An empty array that will store the list of movies retrieved from the OMDB API.
-  const [searchTerm, setSearchTerm] = useState(""); //An empty string that will store the user's input for searching movies.
+  const [movies, setMovies] = useState([]); // An empty array that will store the list of movies retrieved from the OMDB API.
+  const [searchTerm, setSearchTerm] = useState(""); // An empty string that will store the user's input for searching movies.
+
+  const API_KEY = process.env.REACT_APP_OMDB_API_KEY;
+  const API_URL = `https://www.omdbapi.com/?apikey=${API_KEY}`;
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
-    setMovies(data.Search); //pass data search tu ke setMovies
+    setMovies(data.Search); // Pass data.search to setMovies
   };
+
   useEffect(() => {
     searchMovies("Batman");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
@@ -31,7 +34,7 @@ const App = () => {
           value={searchTerm}
           onChange={(event) => setSearchTerm(event.target.value)}
           onKeyDown={(event) => {
-            //allowing user to use the "Enter" button
+            // Allowing user to use the "Enter" button
             if (event.key === "Enter") {
               searchMovies(searchTerm);
             }
@@ -53,7 +56,7 @@ const App = () => {
         <div className="container">
           {movies.map((movie) => (
             /* Map over the 'movies' array and display a MovieCard component for each movie */
-            <MovieCard movie={movie} />
+            <MovieCard key={movie.imdbID} movie={movie} />
           ))}
         </div>
       ) : (
